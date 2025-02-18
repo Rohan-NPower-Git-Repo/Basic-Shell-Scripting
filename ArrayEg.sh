b5bf8e88eaa5fb7924dd16050b35f96f3ed7260d
Rohan-NPower-Git-Repo <rohan.deshpande@npowercanada.ca>
2025-02-18 17:37:26 -0500

    Removed Duplicate Script and Added a definition in ArrayEg

--- ArrayEg.sh
+++ ArrayEg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# forEach loop, which is used exclusively to loop through items present in an array
+
 userNames=("chandler.bing" "monica.geller" "ross.geller" "joey.tribbiani" "phoebe.buffay" "rachel.greene" "gunther")
 # userNames=("Chandler Bing" "Monica Geller" "Ross Geller" "Joey Tribbiani" "Phoebe Buffay" "Rachel Greene" "Gunther")
 # userNames=("Chandler" "Monica" "Ross" "Joey" "Phoebe" "Rachel" "Gunther")
--- getInsUserInfo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo 'User who performed this script : ' $USER >> myLogFile.txt
-echo 'Date  : ' $(date +%F)  >> myLogFile.txt
-echo 'Date_Time : ' $(date +%F_%T)  >> myLogFile.txt
