34319f6c1e2068223b7d4907ba95e49898410999
Rohan-NPower-Git-Repo <rohan.deshpande@npowercanada.ca>
2025-02-20 16:59:05 -0500

    Adding Code for using Switch like Case method

---
+++ CaseEg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Give a Prompt Message and store value in the variable in one line
+read -p "Enter Airport Code: " code
+
+case $code in
+    'YYZ')
+        echo "Toronto : $code - Toronto Pearson Airport";;
+    'YVR')
+        echo "Vancouver : $code - Vancouver International Airport";;
+    'YUL')
+        echo "Montreal : $code - Montréal-Pierre Elliott Trudeau International Airport";;
+    'YHZ')
+        echo "Halifax : $code - Halifax Stanfield International Airport";;
+    *)
+        echo "Incorrect Airport Code";;
+esac
