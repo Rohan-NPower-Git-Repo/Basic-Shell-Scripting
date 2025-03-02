5c73ff52da2b4a48f224f6e8eca58a0fde776896
Rohan-NPower-Git-Repo <rohan.deshpande@npowercanada.ca>
2025-03-02 18:57:00 -0500

    Added a Comment in AutoUpdate and Added uptime in UserMenu

--- AutomaticPackageUpdates.sh
+++ AutomaticPackageUpdates.sh
@@ -6,6 +6,8 @@ echo -e "\n Updating system packages..."
 # Since we don't want to take up more time so let's just run update and not upgrade 
 sudo apt update
 
+# The autoremove command removes unnecessary packages and dependencies then autoclean cleans up cached package files, both with auto confirm (-y).
+
 echo -e "\n Cleaning up unnecessary packages..."
 sudo apt autoremove -y && sudo apt autoclean -y
 
--- CreateUserMenu.sh
+++ CreateUserMenu.sh
@@ -5,7 +5,8 @@ while true; do
     echo "1. Show Disk Usage"
     echo "2. Show Running Processes"
     echo "3. Show Logged-in Users"
-    echo "4. Exit"
+    echo "4. Show Server Uptime"
+    echo "5. Exit"
     
     read -p "Choose an option: " option
 
@@ -13,7 +14,8 @@ while true; do
         1) df -h ;;
         2) ps aux ;;
         3) w ;;
-        4) echo "Exiting..."; exit ;;
+        4) uptime ;;
+	5) echo "Exiting..."; exit ;;
         *) echo -e "\n Invalid option, please try again." ;;
     esac
 done
